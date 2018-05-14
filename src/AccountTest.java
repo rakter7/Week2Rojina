1c99096f2b724ff6dac9250968d54943de39c3b8
300756946 <rakter7@my.centennialcollege.ca>
2018-05-14 17:47:38 -0400

    Modified AccountTest class

--- src/AccountTest.java
+++ src/AccountTest.java
@@ -7,9 +7,18 @@ public class AccountTest {
 
         //crate an instance of Account
         Account myAccount = new Account();
-
+        String accName = input.nextLine(); //Read input and save in accName
+        myAccount.setName(accName);
         //display the default account info
         System.out.printf("Initial Name is: %s%n%n", myAccount.getName());
 
+        //Prompt the user for an account name
+        System.out.printf("Please enter the user name: ");
+        String accName1 = input.nextLine(); //Read input and save in accName
+        myAccount.setName(accName1);
+        System.out.println();
+
+        //Display account information
+        System.out.printf("Name of my  account is : %s%n",myAccount.getName());
     }
 }
